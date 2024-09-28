0c4fe7d3b04652a8da432808404c3e92dc57c7c4
Marek Michalski <marek.michalski@me.com>
2024-09-28 10:02:39 +0200

    Initial Commit

---
+++ Battleship/BattleshipApp.swift
@@ -0,0 +1,17 @@
+//
+//  BattleshipApp.swift
+//  Battleship
+//
+//  Created by Marek Michalski on 28/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BattleshipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BattleshipTests/BattleshipTests.swift
@@ -0,0 +1,17 @@
+//
+//  BattleshipTests.swift
+//  BattleshipTests
+//
+//  Created by Marek Michalski on 28/09/2024.
+//
+
+import Testing
+@testable import Battleship
+
+struct BattleshipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
